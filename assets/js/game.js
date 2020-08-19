6a4b0f8595a44a62b1473de9130a4527250263f9
Garlic Fiend <suzannaakins@gmail.com>
2020-08-19 13:27:43 -0700

    in shop function, switch statement uses integer instead of strings. data type conversion using parseInt method to convert string to integer

--- assets/js/game.js
+++ assets/js/game.js
@@ -149,21 +149,19 @@ var endGame = function () {
 var shop = function () {
     //ask player what they'd like to do
     var shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your sttack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter 1 for REFILL, 2 for UPGRADE, or 3 to LEAVE STORE."
     );
+    shopOptionPrompt = parseInt(shopOptionPrompt);
     //use switch to carry out action
     switch (shopOptionPrompt) {
-        case "REFILL": //new case
-        case "refill":
+        case 1: //refill Health
             playerInfo.refillHealth();
             break;
-        case "UPGRADE": //new case
-        case "upgrade":
+        case 2: // upgrade Attack
             playerInfo.upgradeAttack();
             break;
 
-        case "LEAVE": //new case
-        case "leave":
+        case 3: //Leaves the Store
             window.alert("Leaving the store");
             //do nothing, so function will END
             break;
